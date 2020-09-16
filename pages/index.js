b5f3634db5aa864085fdf7cb8658cc96c776aa90
Mateusz Wszola <wszola.m@gmail.com>
2020-09-16 20:40:01 +0200

    improve header layout

--- pages/index.js
+++ pages/index.js
@@ -21,21 +21,23 @@ export default function Home() {
       </Head>
       <Box h="100vh">
         <Flex as="main" h="100%" direction="column">
-          <Flex justify="space-between" align="center" w="full" maxW="1280px" mx="auto" p={2}>
-            <Box as="h1" display="flex" alignItems="center" fontSize="2xl" fontWeight="bold">
-              <SiOpenstreetmap />
-              <Text as="span" ml={1} textTransform="uppercase" letterSpacing="tighter">
-                GuestMap
-              </Text>
-            </Box>
-            <Box ml={2}>
-              <IconButton
-                onClick={toggleColorMode}
-                aria-label="Toggle theme"
-                icon={colorMode === 'dark' ? <FiMoon /> : <FiSun />}
-              />
-            </Box>
-          </Flex>
+          <Box as="header" p={2}>
+            <Flex w="full" maxW="1280px" mx="auto" justify="space-between" align="center">
+              <Box as="h1" display="flex" alignItems="center" fontSize="2xl" fontWeight="bold">
+                <SiOpenstreetmap />
+                <Text as="span" ml={1} textTransform="uppercase" letterSpacing="tighter">
+                  GuestMap
+                </Text>
+              </Box>
+              <Box ml={[2, 0]}>
+                <IconButton
+                  onClick={toggleColorMode}
+                  aria-label="Toggle theme"
+                  icon={colorMode === 'dark' ? <FiMoon /> : <FiSun />}
+                />
+              </Box>
+            </Flex>
+          </Box>
           <Map />
         </Flex>
       </Box>
